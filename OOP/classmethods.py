5f087e0bc476509f0deb1533b068d7d0749127dc
hoandy445 <146103279+hoandy445@users.noreply.github.com>
2024-04-13 23:47:57 -0500

    practice 4/13

--- OOP/classmethods.py
+++ OOP/classmethods.py
@@ -28,3 +28,86 @@
 
 # print('Employee count:', Employee.get_employee_count())
 
+
+# class Shape:
+#     shape_count = 0
+
+#     def __init__(self,name,*args):
+#         self.name = name
+#         self.sides = [arg for arg in args]
+
+#         Shape.shape_count += 1
+
+
+#     def display_shape(self):
+#         print(f'''
+# Shape: {self.name}
+# Sides: {self.sides}
+# ''')
+
+#     @classmethod
+#     def get_shape_count(cls):
+#         print('Total number of shapes:',end=' ')
+#         return cls.shape_count
+
+
+    
+# shape1 = Shape('Square', 20,20)
+# shape2 = Shape('Rectangle', 20,30)
+
+# shape1.display_shape()
+# print(shape1.get_shape_count())
+
+
+class OnlineStore:
+    store_inventory = {}
+    store_sales = 0
+    store_count = 0
+    item_id_counter = 1000
+
+    def __init__(self,name):
+        self.name = name
+        OnlineStore.store_count += 1
+
+    def add_item(self,item_name,price,quantity):
+        # create unique item ID for each item added
+        OnlineStore.item_id_counter += 1 
+
+        # update store_inventory dictionary with item information, item_id_counter as key
+        OnlineStore.store_inventory.update(
+            {OnlineStore.item_id_counter:
+                {'Item name':item_name,
+                'Price':price,
+                'Quantity':quantity} 
+            })
+
+    def sell_item(self,item_id,quantity):
+        # update sales        
+        price = OnlineStore.store_inventory[item_id]['Price']
+        sales = price * quantity
+        OnlineStore.store_sales += sales
+
+
+        # update item quantity
+        OnlineStore.store_inventory[item_id]['Quantity'] = OnlineStore.store_inventory[item_id]['Quantity'] - quantity
+
+    @classmethod
+    def get_inventory(cls):
+        return cls.store_inventory
+    
+    @classmethod
+    def get_total_sales(cls):
+        return cls.store_sales
+
+    @classmethod
+    def get_store_count(cls):
+        return cls.store_count
+
+misfortune_seafood = OnlineStore('Misfortune Seafood')
+
+misfortune_seafood.add_item('300/UP KING CRAB',150.00,500)
+misfortune_seafood.sell_item(1001,10)
+print(OnlineStore.store_sales)
+print(misfortune_seafood.get_inventory())
+print(misfortune_seafood.get_total_sales())
+print(misfortune_seafood.get_store_count())
